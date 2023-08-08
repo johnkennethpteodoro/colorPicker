44d4285736e772821d7d28d760add13ceb2317b9
jkqstrike <johnkennethpteodoro@gmail.com>
2023-08-08 23:44:21 +0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,6 @@ function App() {
 		setSelectedColor(color);
 	};
 
-	function getContrastingTextColor(hexColor) {
-		const r = parseInt(hexColor.slice(0, 2), 16);
-		const g = parseInt(hexColor.slice(2, 4), 16);
-		const b = parseInt(hexColor.slice(4, 6), 16);
-		const relativeLuminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-		return relativeLuminance > 128 ? "black" : "white";
-	}
-
 	return (
 		<>
 			<div className="container">
@@ -37,8 +29,8 @@ function App() {
 							<div className="row">
 								{colors.map((color) => (
 									<div className="col-sm-3 text-center mb-4" key={color.id}>
-										<button className="btn btn-primary" onClick={() => colorHandle(color)}>
-											{color.color_code}
+										<button className="btn btn-primary" style={{ backgroundColor: `#${color.hex_code}` }} onClick={() => colorHandle(color)}>
+											<span className="colorInvert">{color.color_code}</span>
 										</button>
 										<h6 className="mt-1 colorName">{color.name}</h6>
 									</div>
@@ -47,12 +39,12 @@ function App() {
 						)}
 					</div>
 					<div className="col-sm-5 mt-5 me-4">
-						<div className="boxColor position-relative" style={{ backgroundColor: selectedColor ? `#${selectedColor.hex_code}` : "white" }}>
+						<div className="boxColor position-relative" style={{ backgroundColor: selectedColor ? `#${selectedColor.hex_code}` : "#FAFAFA", border: selectedColor ? "none" : "dashed 3px #EEEEEE" }}>
 							<div className="position-absolute top-50 start-50 translate-middle">
-								<h6 className="colorName" style={{ color: selectedColor ? getContrastingTextColor(selectedColor.hex_code) : "black" }}>
-									<span>{selectedColor ? selectedColor.color_code : null}</span> - {selectedColor ? selectedColor.name : null}
+								<h6 className="colorName colorInvert">
+									<span>{selectedColor ? `${selectedColor.color_code} - ${selectedColor.name}` : "PREVIEW"}</span>
 								</h6>
-								<h6 style={{ color: selectedColor ? getContrastingTextColor(selectedColor.hex_code) : "black" }}>{selectedColor ? selectedColor.hex_code : null}</h6>
+								<h6 className="colorInvert">{selectedColor ? `( #${selectedColor.hex_code} )` : null}</h6>
 							</div>
 						</div>
 					</div>
